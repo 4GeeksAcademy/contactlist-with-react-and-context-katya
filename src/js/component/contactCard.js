bc907088ac9ff9b3717796c76c5869762a03c9c0
t0kyolover <144109471+t0kyolover@users.noreply.github.com>
2024-01-21 20:13:46 +0000

    added button interface to add new contact linked with a path

--- src/js/component/contactCard.js
+++ src/js/component/contactCard.js
@@ -5,7 +5,7 @@ export const ContactCard = () => {
   const { store, actions } = useContext(Context);
 
   return (
-    <div className="card mb-3" style={{ width: "1000px" }}>
+    <div className="card mb-3" style={{ width: "1030px" }}>
       <div className="row g-0">
         <div className="col-md-4 d-flex justify-content-center">
           <img
@@ -18,7 +18,7 @@ export const ContactCard = () => {
         <div className="col-md-6">
           <div className="card-body">
             <h5 className="card-title mb-3">{store.contact[0].name}</h5>
-            <p className="card-text text-muted">
+            <div className="card-text text-muted">
               <p className="mb-1">
                 {" "}
                 <i className="fas fa-map-marker-alt me-2"></i>
@@ -34,7 +34,7 @@ export const ContactCard = () => {
                 <i className="fas fa-envelope me-2"></i>
                 {store.contact[0].email}
               </p>
-            </p>
+            </div>
           </div>
         </div>
         <div className="col-md-2 mt-3 d-flex justify-content-end align-items-start">
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { ContactCard } from "../component/contactCard";
-
+import { Link } from "react-router-dom";
 
 import "../../styles/home.css";
 
 export const Home = () => (
-	<div className="d-flex justify-content-center mt-5">
-		<ContactCard />
-	</div>
-)
+  <div>
+    <nav className="navbar navbar-light mb-3 ">
+      <div className="col-4 d-flex justify-content-end">
+        <Link to="/demo">
+          <button className="btn btn-success me-5">Add new contact</button>
+        </Link>
+      </div>
+    </nav>
+    <div className="d-flex justify-content-center mt-3">
+      <ContactCard />
+    </div>
+  </div>
+);
